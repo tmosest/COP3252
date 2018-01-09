37ec689cb93a14c6a8a168d6b46a677ee0f105fe
Tyler Moses <tmosest@gmail.com>
2018-01-08 20:15:53 -0500

    Solved A1

---
+++ A1/Assignment1.java
@@ -0,0 +1,15 @@
+/*
+Moses, Tyler
+COP-3252
+Assignment 1
+01/08/2018
+*/
+public class Assignment1
+{
+  // My main method
+  public static void main(String[] args)
+  {
+    System.out.println("Welcome to COP-3252 Java Programming!");
+  }
+}
+
